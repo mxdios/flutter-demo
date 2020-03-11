a0137dcb8d84fb867bb1dce1697f16d375fcd1ac
mark <mark.miao@inspiry.cn>
2020-03-11 18:20:11 +0800

    demo

--- first_demo/lib/main.dart
+++ first_demo/lib/main.dart
@@ -10,16 +10,55 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final name = WordPair.random();
+    // final name = WordPair.random();
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text(name.asCamelCase),
-        ),
+      home: RandomWorld()
+    );
+  }
+}
+
+class RandomWorld extends StatefulWidget {
+  @override
+  WorldState createState() => WorldState();
+}
+
+//创建一个State类
+class WorldState extends State<RandomWorld> {
+  final _worldArray = <WordPair>[];
+  final _fontStyle = const TextStyle(fontSize: 10.0,);
+
+  Widget _listView() {
+    return ListView.builder(
+      itemBuilder: (context, i) {
+        if (i.isOdd) {
+          return Divider();
+        }
+        final index = i ~/ 2;
+        if (index >= _worldArray.length) {
+          _worldArray.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_worldArray[index]);
+      }
+    );
+  }
+
+  Widget _buildRow(WordPair name) {
+    return ListTile(
+      title: Text(
+        name.asPascalCase,
+        style: _fontStyle,
       ),
     );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar (
+        title: Text('Home'),
+      ),
+      body: _listView(),
+    );
+  }
+
 }
